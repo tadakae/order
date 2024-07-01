b30bfb83186e1da5a9446a0739bbaed1cc2b8cfb
roma <ramazan.sapar003@gmail.com>
2024-07-01 17:05:26 +0300

    v1

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<!-- App.vue -->
+<template>
+  <div id="app">
+    <MenuItems />
+  </div>
+</template>
+
+<script>
+import MenuItems from './components/MenuItems.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MenuItems
+  }
+};
+</script>
+
+<style>
+/* Глобальные стили, если необходимы */
+</style>
---
+++ src/components/UsersList.vue
@@ -0,0 +1,45 @@
+<!-- UsersList.vue -->
+<template>
+  <div class="">
+    <ul>
+      <li v-for="user in users" :key="user.id">
+      Заказчик:  {{ user.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+export default {
+  name: 'UsersList',
+  setup() {
+    // Создаем реактивную переменную для хранения данных о пользователях
+    const users = ref([]);
+
+    // Функция для загрузки данных с сервера с помощью Axios
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('https://8a5d97df2ab05859.mokky.dev/users'); // замените на ваш URL API
+        users.value = response.data;
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    // Хук, вызываемый после монтирования компонента
+    onMounted(fetchUsers);
+
+    // Возвращаем данные для использования в шаблоне
+    return {
+      users
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* CSS стили для компонента, если необходимы */
+</style>
